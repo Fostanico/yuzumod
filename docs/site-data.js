5d2dce3d668bfd9cd572631985664d42622f919f
Fostanico <106441517+Fostanico@users.noreply.github.com>
2026-08-11 21:35:31 +0800

    网站一不小心删了

---
+++ docs/site-data.js
@@ -0,0 +1,6 @@
+// Edit these three values and publish this file to update the public website.
+window.YUZUTURE_SITE_DATA = Object.freeze({
+  updateDate: "2026.7.15",
+  supportedVersion: "1.19.2.0",
+  modVersion: "1.181"
+});
